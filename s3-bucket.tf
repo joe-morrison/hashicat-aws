9700fee430b2285b789e3ba534e5707ae9380fc8
Joe Morrison <joseph.morrison@live.com>
2022-04-06 09:57:01 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/Joe-Private/s3-bucket/aws"
   version = "2.8.0"
   # insert required variables here
-  bucket_prefix = "Joe-Private"
+  bucket_prefix = "joe-private"
 }
